bdf5c60fb018ad04969a51f9fa61f83b0458af78
emil konstantinovitz <emil konstantinovitz>
2026-04-21 15:02:14 +0200

    pre built binary flag

--- flake.nix
+++ flake.nix
@@ -8,16 +8,9 @@
   outputs = {self, nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
-    custom-packages = import ./packages {inherit pkgs;};
   in {
     nixosModules.default = import ./modules;
 
-    packages.${system} = {
-      inherit (custom-packages)
-        linpeas winpeas netcat-win go-windapsearch seclists
-        mimikatz sharphound powerview godpotato ligolo-ng-binaries
-        runascs custom-scripts rshells sliver pspy rockyou
-        webshells vulnx pywhisker arsenal;
-    };
+    packages.${system} = import ./packages {inherit pkgs;};
   };
 }
--- packages/default.nix
+++ packages/default.nix
@@ -1,43 +1,56 @@
-{pkgs, binaryServer ? "http://localhost:8080", allowPrebuilt ? true}: let
-  linpeas = pkgs.callPackage ./linpeas {};
-  winpeas = pkgs.callPackage ./winpeas {};
-  netcat-win = pkgs.callPackage ./netcat-win {};
-  go-windapsearch = pkgs.callPackage ./go-windapsearch {};
-  seclists = pkgs.callPackage ./seclists {};
-  mimikatz = pkgs.callPackage ./mimikatz {};
-  sharphound = pkgs.callPackage ./sharphound {};
-  rubeus = pkgs.callPackage ./rubeus {inherit binaryServer;};
-  powerview = pkgs.callPackage ./powerview {};
-  godpotato = pkgs.callPackage ./godpotato {};
-  ligolo-ng-binaries = pkgs.callPackage ./ligolo-ng {};
-  runascs = pkgs.callPackage ./runascs {};
-  custom-scripts = pkgs.callPackage ./custom-scripts {};
-  rshells = pkgs.callPackage ./rshells {};
-  sliver = pkgs.callPackage ./sliver {};
-  pspy = pkgs.callPackage ./pspy {};
-  rockyou = pkgs.callPackage ./rockyou {};
-  webshells = pkgs.callPackage ./webshells {};
-  vulnx = pkgs.callPackage ./vulnx {};
-  pywhisker = pkgs.callPackage ./pywhisker {};
+{
+  pkgs,
+  binaryServer ? "http://localhost:8080",
+  allowPrebuilt ? true,
+}: let
+  # define once, that's it
+  packages = {
+    linpeas = pkgs.callPackage ./linpeas {};
+    winpeas = pkgs.callPackage ./winpeas {};
+    netcat-win = pkgs.callPackage ./netcat-win {};
+    go-windapsearch = pkgs.callPackage ./go-windapsearch {};
+    seclists = pkgs.callPackage ./seclists {};
+    mimikatz = pkgs.callPackage ./mimikatz {};
+    sharphound = pkgs.callPackage ./sharphound {};
+    rubeus = pkgs.callPackage ./rubeus {inherit binaryServer;};
+    powerview = pkgs.callPackage ./powerview {};
+    godpotato = pkgs.callPackage ./godpotato {};
+    ligolo-ng-binaries = pkgs.callPackage ./ligolo-ng {};
+    runascs = pkgs.callPackage ./runascs {};
+    custom-scripts = pkgs.callPackage ./custom-scripts {};
+    rshells = pkgs.callPackage ./rshells {};
+    sliver = pkgs.callPackage ./sliver {};
+    pspy = pkgs.callPackage ./pspy {};
+    rockyou = pkgs.callPackage ./rockyou {};
+    webshells = pkgs.callPackage ./webshells {};
+    vulnx = pkgs.callPackage ./vulnx {};
+    pywhisker = pkgs.callPackage ./pywhisker {};
+  };
 
   allowed = pkg: allowPrebuilt || !(pkg.passthru.prebuilt or false);
 
-  arsenalPackages = [
-    linpeas winpeas pspy godpotato winpeas runascs
-    mimikatz sharphound rubeus powerview
-    ligolo-ng-binaries netcat-win
-    rshells webshells
-    seclists rockyou
+  arsenalPackages = with packages; [
+    linpeas
+    winpeas
+    pspy
+    godpotato
+    runascs
+    mimikatz
+    sharphound
+    rubeus
+    powerview
+    ligolo-ng-binaries
+    netcat-win
+    rshells
+    webshells
+    seclists
+    rockyou
   ];
-in {
-  inherit
-    linpeas winpeas netcat-win go-windapsearch seclists
-    mimikatz sharphound rubeus powerview godpotato
-    ligolo-ng-binaries runascs custom-scripts rshells sliver
-    pspy rockyou webshells vulnx pywhisker;
-
-  arsenal = pkgs.symlinkJoin {
-    name = "arsenal";
-    paths = builtins.filter allowed arsenalPackages;
-  };
-}
+in
+  packages
+  // {
+    arsenal = pkgs.symlinkJoin {
+      name = "arsenal";
+      paths = builtins.filter allowed arsenalPackages;
+    };
+  }
